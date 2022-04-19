d31d1f74061c83b7cb78bb26544b82e85bf1025f
dokgo <kdk5go@gmail.com>
2022-04-19 23:21:48 +0900

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import date
 def main():
     access_key = os.environ['MY_GITHUB_TOKEN']
     repo = github(access_key, 'study-github-actions')
-    push_issue(repo, 'a', date.today())
+    push_issue(repo, 'a', "test")
 
 
 if __name__ == "__main__":
